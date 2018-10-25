eef3c6c0cf9a7802fa7be1ecc3cf8a260fb8f9fa
ToQuery <toquery@qq.com>
2018-10-25 21:54:55 +0800

    修改jpa相关配置

--- clever-framework-dao/src/main/java/io/github/toquery/framework/dao/entity/AppBaseEntity.java
+++ clever-framework-dao/src/main/java/io/github/toquery/framework/dao/entity/AppBaseEntity.java
@@ -1,9 +1,12 @@
 package io.github.toquery.framework.dao.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import io.github.toquery.framework.core.constant.AppPropertiesDefault;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
@@ -35,27 +38,31 @@ public class AppBaseEntity implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @CreatedBy
-    @Column(name = "create_user_id", length = 32, updatable = false)
-    private String createUserId;
+    @Column(name = AppPropertiesDefault.JPA_COLUMN_CREATE_BY, length = 32, updatable = false)
+    private String createdBy;
 
 
     @CreatedDate
+    @CreationTimestamp
     @Temporal(TemporalType.TIMESTAMP)
-    @DateTimeFormat(pattern = AppPropertiesDefault.DATE_TIME_PATTERN)
-    @Column(name = "create_time", updatable = false, nullable = false)
-    private Date createDatetime;
+    //@DateTimeFormat(pattern = AppPropertiesDefault.DATE_TIME_PATTERN)
+    @JsonFormat(pattern = AppPropertiesDefault.DATE_TIME_PATTERN, locale = "zh", timezone = "GMT+8")
+    @Column(name = AppPropertiesDefault.JPA_COLUMN_CREATE_DATE, updatable = false, nullable = false)
+    private Date createdDate;
 
 
     @LastModifiedBy
-    @Column(name = "last_update_user_id", length = 32)
-    private String lastUpdateUserId;
+    @Column(name = AppPropertiesDefault.JPA_COLUMN_LAST_MODIFIED_BY, length = 32)
+    private String lastModifiedBy;
 
 
     @LastModifiedDate
+    @UpdateTimestamp
     @Temporal(TemporalType.TIMESTAMP)
-    @Column(name = "last_update_time", nullable = false)
-    @DateTimeFormat(pattern = AppPropertiesDefault.DATE_TIME_PATTERN)
-    private Date lastUpdateDatetime;
+    @Column(name = AppPropertiesDefault.JPA_COLUMN_LAST_MODIFIED_DATE, nullable = false)
+    @JsonFormat(pattern = AppPropertiesDefault.DATE_TIME_PATTERN, locale = "zh", timezone = "GMT+8")
+    //@DateTimeFormat(pattern = AppPropertiesDefault.DATE_TIME_PATTERN)
+    private Date lastModifiedDate;
 
     /**
      * 是否删除：1已删除；0未删除
