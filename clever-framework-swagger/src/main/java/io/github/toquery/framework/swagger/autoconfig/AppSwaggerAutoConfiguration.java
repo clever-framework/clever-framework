a4dc7346b3ff00a95570c4ec2d914e68d27f8f3b
ToQuery <toquery@qq.com>
2019-07-12 13:37:57 +0800

    1.0.6-SNAPSHOT

---
+++ clever-framework-swagger/src/main/java/io/github/toquery/framework/swagger/autoconfig/AppSwaggerAutoConfiguration.java
@@ -0,0 +1,16 @@
+package io.github.toquery.framework.swagger.autoconfig;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+
+
+/**
+ * @author toquery
+ * @version 1
+ */
+@Slf4j
+@Configuration
+public class AppSwaggerAutoConfiguration {
+
+
+}
